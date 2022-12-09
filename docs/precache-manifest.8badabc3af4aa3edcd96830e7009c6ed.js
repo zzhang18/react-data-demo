c15ad91b5302bbb349602d023d0c925a6ef6e059
zzhang18 <zzhan18@syr.edu>
2022-12-09 11:13:16 +0800

    pub

---
+++ docs/precache-manifest.8badabc3af4aa3edcd96830e7009c6ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749d96a7fb535eb8d602118f619c1d35",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a81fc16010b5d35eae1",
+    "url": "./static/css/main.5b253a5a.chunk.css"
+  },
+  {
+    "revision": "1dca84652efde8598aeb",
+    "url": "./static/js/2.fdf6f808.chunk.js"
+  },
+  {
+    "revision": "5a81fc16010b5d35eae1",
+    "url": "./static/js/main.d9260678.chunk.js"
+  },
+  {
+    "revision": "5631527d557d539fc0ba",
+    "url": "./static/js/runtime~main.a183acfc.js"
+  },
+  {
+    "revision": "837e99ea750ab1684ed2661cef36cc55",
+    "url": "./static/media/bg.837e99ea.png"
+  }
+]);
